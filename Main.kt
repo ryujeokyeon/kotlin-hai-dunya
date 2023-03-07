88038011a24cbad23dccabd15014e428d40f4ea3
ryujeokyeon <rjeokyeon.gmail.com>
2023-03-07 08:38:09 +0700

    first commit

---
+++ Main.kt
@@ -0,0 +1,14 @@
+/*
+---- running----
+=> terminal
+=> kotlin path\nama_file.kt -include-runtime -d path\out\nama_file.jar
+=> java -jar path\out\nama_file.jar
+
+kotlinc D:\Belajar\Kotlin\day-01\Main.kt -include-runtime -d D:\Belajar\Kotlin\day-01\out\Main.jar
+java -jar D:\Belajar\Kotlin\day-01\out\Main.jar
+
+*/
+
+fun main() {
+    println("hai, dunya!")
+}
